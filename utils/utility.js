5d8efa91e47782f498899b127732d17aa6a3c073
aezed <aezedr@gmail.com>
2016-07-08 13:39:17 -0700

    Improve formatting of recipe titles from Cooks.com, change default image, improve error handling

--- utils/utility.js
+++ utils/utility.js
@@ -3,11 +3,24 @@ const fs = require('fs');
 // Utility functions
 
 // Store data as a JSON object in a file
-exports.saveData = (recData, recFile) => {
+module.exports.saveData = (recData, recFile) => {
   // Convert recipe data into an array of JSON objects
   const recipeJSON = JSON.stringify(recData, null, 2);
   // Write the recipes to recFile
   fs.writeFileSync(recFile, recipeJSON);
+};
+
+// Reformat recipe title to capitalize first letter of words
+module.exports.firstCap = (recTitle) => {
+  let newTitle = '';
+  const titleWords = recTitle.split(' ');
+
+  titleWords.forEach(word => {
+    const newWord = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    newTitle += `${newWord} `;
+  });
+
+  return newTitle;
 };
 
 // Combine recipe files
